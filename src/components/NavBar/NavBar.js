7b2faef61f97617f9211ae0dc180964eb8274c9f
hut1 <hut1@tcd.ie>
2023-10-18 16:51:46 +0100

    update some page components

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,8 +1,15 @@
 // NavBar.js
-import React from 'react';
+import React, { useState }from 'react';
 import './NavBar.css';
+import userIcon from "../../assets/images/user.svg";
 
 function NavBar({ currentPage }) {
+    const [isLogged, setIsLogged] = useState(false); // 登录状态
+    const [showDropdown, setShowDropdown] = useState(false); // 显示下拉菜单
+    const [showPopup, setShowPopup] = useState(false); // 显示弹出框
+    const [isLoginMode, setIsLoginMode] = useState(true); // 登录或注册模式
+
+
     return (
         <div className="navbar">
             <div className="rectangle4">
@@ -16,7 +23,34 @@ function NavBar({ currentPage }) {
                     <a href="/booking" className={currentPage === 'Booking a room' ? 'active' : ''}>Booking a room</a>
                     <a href="/contact" className={currentPage === 'Contact Us' ? 'active' : ''}>Contact Us</a>
                 </div>
-                <div className="user-avatar"></div>
+                <div className="user-avatar" onMouseEnter={() => setShowDropdown(true)} onMouseLeave={() => setShowDropdown(false)}>
+                    <img src={userIcon} alt="User" />
+                    {showDropdown && !isLogged &&
+                    <div className="dropdown-menu">
+                        <button onClick={() => {setIsLoginMode(true); setShowPopup(true);}}>Login</button>
+                        <button onClick={() => {setIsLoginMode(false); setShowPopup(true);}}>Register</button>
+                    </div>
+                    }
+                </div>
+                {showPopup &&
+                <div className="auth-popup">
+                    {isLoginMode ?
+                        <>
+                            <input type="email" placeholder="Email" />
+                            <input type="password" placeholder="Password" />
+                            <button>Login</button>
+                        </>
+                        :
+                        <>
+                            <input type="email" placeholder="Email" />
+                            <input type="password" placeholder="Password" />
+                            <button>Register</button>
+                        </>
+                    }
+                    <button onClick={() => setShowPopup(false)}>Close</button>
+                </div>
+                }
+
             </div>
             <div className="rectangle5"></div>
         </div>
--- src/components/WhatDoYouNeed/WhatDoYouNeed.js
+++ src/components/WhatDoYouNeed/WhatDoYouNeed.js
@@ -8,20 +8,24 @@ export const WhatDoYouNeed = () => {
             <div className="WDYN-box">
                 <h2>WHAT DO YOU NEED?</h2>
                 <div className="input-group">
-                    <select className="room-type">
-                        <option value="" disabled selected>Select a RoomType</option>
-                        <option value="bronze" >Bronze</option>
-                        <option value="silver">Silver</option>
-                        <option value="gold">Gold</option>
-                    </select>
-                    <div className="date-input-wrapper">
-                        <input type="date" className="start-date" id="startDate" />
-                        <label htmlFor="startDate" className="date-placeholder">Start Date</label>
+                    <div className="input-wrapper">
+                        <label htmlFor="roomType" className="input-label">Select a Room</label>
+                        <select className="room-type" id="roomType">
+                            <option value="" disabled selected>Select a RoomType</option>
+                            <option value="bronze">Bronze</option>
+                            <option value="silver">Silver</option>
+                            <option value="gold">Gold</option>
+                        </select>
                     </div>
 
-                    <div className="date-input-wrapper">
+                    <div className="input-wrapper">
+                        <label htmlFor="startDate" className="input-label">Start Date</label>
+                        <input type="date" className="start-date" id="startDate" />
+                    </div>
+
+                    <div className="input-wrapper">
+                        <label htmlFor="endDate" className="input-label">End Date</label>
                         <input type="date" className="end-date" id="endDate" />
-                        <label htmlFor="endDate" className="date-placeholder">End Date</label>
                     </div>
                 </div>
                 <button className="search-btn">Search</button>
